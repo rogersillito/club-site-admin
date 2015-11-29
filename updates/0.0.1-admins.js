d6251e02828f692be766bf176fc95567a564ee22
rogersillito <rogersillito@gmail.com>
2015-11-29 22:49:49 +0000

    updated email

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -9,7 +9,7 @@
 
 exports.create = {
 	User: [
-		{ 'name.first': 'Admin', 'name.last': 'User', email: 'rogersillito@gmail.com', password: 'admin', isAdmin: true }
+		{ 'name.first': 'Admin', 'name.last': 'User', email: 'roginthebog@gmail.com', password: 'admin', isAdmin: true }
 	]
 };
 
