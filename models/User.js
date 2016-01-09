75a2d906556d03e5fc6709798818f86fc1cff073
rogersillito <rogersillito@gmail.com>
2016-01-09 19:06:10 +0000

    initial work on a screen to show meeting result detail

--- models/User.js
+++ models/User.js
@@ -13,7 +13,8 @@ User.add({
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
+	isAdmin: { type: Boolean, label: 'Has admin access', index: true },
+	isContributor: { type: Boolean, label: 'Has contributor access', index: true }
 });
 
 // Provide access to Keystone
