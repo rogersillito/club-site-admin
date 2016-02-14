6ef77c0e160a82f142a7dc76803434a33dc8effd
rogersillito <rogersillito@gmail.com>
2016-02-14 20:57:54 +0000

    got the start of some month-based results filtering done

--- routes/views/results.js
+++ routes/views/results.js
@@ -1,4 +1,7 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+    moment = require('moment');
+
+var MeetingResult = keystone.list('MeetingResult');
 
 exports = module.exports = function(req, res) {
 	
@@ -8,11 +11,43 @@ exports = module.exports = function(req, res) {
 	// Set locals
 	locals.section = 'results';
 
+    view.on('init', function(next) {
+
+        var criteria = {'isPublished': true};
+
+        MeetingResult.model.findOne(criteria, 'date').sort({date: 'desc'}).exec(function(err, latest) {
+            
+			      if (err || latest === null) {
+				        return next(err);
+			      }
+
+            var latestDate = moment(latest.date);
+            locals.currentYear = latestDate.year();
+            var monthIdx = latestDate.month();
+            locals.currentMonth = latestDate.format('MMMM');
+
+            //TODO: make sure these bring out the right start/end dates
+            var displayStartDate = new moment().year(locals.currentYear).month(monthIdx).date(1);
+            var displayEndDate = new moment().year(locals.currentYear).month(monthIdx + 1).date(1);
+            console.log(displayStartDate.format('D M YYYY'), displayEndDate.format('D M YYYY'));
+            criteria['date'] = {
+                $gte: displayStartDate,
+                $lt: displayEndDate
+            };
+
+			      // locals.data.tags = results;
+            next();
+        });
+
+	      view.query('results', MeetingResult.model.find(criteria).sort('-date'));
+
+    });
+
     //TODO: something like this.....
-	  // view.query('upcomingMeetup',
-		//            Meetup.model.findOne()
+	  // view.query('latestResult',
+		//            MeetingResult.model.findone()
 		// 	         .where('state', 'active')
-		// 	         .sort('-startDate')
+		// 	         .sort('-startdate')
 	  //            , 'talks[who]');
 	  
 	  // view.query('pastMeetups',
@@ -26,7 +61,6 @@ exports = module.exports = function(req, res) {
     // });
 	
 	// Load the results by sortOrder
-	view.query('results', keystone.list('MeetingResult').model.find().sort('sortOrder'));
 	
 	// Render the view
 	view.render('results');
