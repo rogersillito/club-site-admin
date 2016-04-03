ad6ab2567c0177517daa97f4ede87a20fc1d9f3d
rogersillito <rogersillito@gmail.com>
2016-04-03 21:41:49 +0100

    added react scripts (CDN) and a script to contain react code - include in public views

---
+++ public/js/site/results.js
@@ -0,0 +1,24 @@
+//TODO: react compononentize this
+//TODO: where does the menu go? also?
+
+/*
+    <div class="container">
+	  <h1>Race Results</h1>
+    <h2>{{displayMonth}} {{displayYear}}</h2>
+    </div>
+    <div class="container">
+	  {{#if results}}
+{{#each results}}
+		<h3>{{nameOrLocation}}
+		<div class="pull-right text-muted">{{date this.date format='dddd Do MMMM YYYY'}}</div>
+		</h3>
+    {{{resultHtml}}}
+{{/each}}
+{{else}}
+    <div class="container">
+    <h1>Results</h1>
+    </div>
+	  <h3 class="text-muted">There are no results for this month.</h3>
+	  {{/if}}
+</div>
+ */
