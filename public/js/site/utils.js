2ebe85ce806862ea04edbdbe44ee2551b21c95fe
rogersillito <rogersillito@gmail.com>
2016-04-09 17:26:44 +0100

    added styles and js to support collapsable bootstrap list-groups

---
+++ public/js/site/utils.js
@@ -0,0 +1,8 @@
+$(function() {
+    // collapsable list group icon behaviour
+    $('.list-group-item').on('click', function() {
+        $('.glyphicon', this)
+            .toggleClass('glyphicon-chevron-right')
+            .toggleClass('glyphicon-chevron-down');
+    });
+});
