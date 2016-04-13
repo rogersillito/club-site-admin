517fa669ce8cf770e22905881b55c2d4dfad8bc2
rogersillito <rogersillito@gmail.com>
2016-04-13 17:27:09 +0100

    working on getting grunt/jasmine to work - added commented code + git ignore for spec runner

--- grunt/jasmine.js
+++ grunt/jasmine.js
@@ -1,5 +1,6 @@
 module.exports = {
     jasmine: {
+        // src: 'lib/**/*.js', // don't think this is necessary, but try adding it in just in case
         options: {
             keepRunner: true,
             template: require('grunt-template-jasmine-nml'),
