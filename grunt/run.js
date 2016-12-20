3c398d895cb7114c1dd3837e7929c68aee157469
rogersillito <rogersillito@gmail.com>
2016-12-20 16:12:39 +0000

    removing dirs previously ignored via gitignore for deploy scenario

--- grunt/run.js
+++
@@ -1,5 +0,0 @@
-// Invite external commands into your grunt process with three tasks run, wait and stop.
-// https://github.com/spalger/grunt-run#the-run-task
-module.exports = {
-
-};
--- grunt/wget.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // install non-bower js dependencies into public/lib
-    install: {
-        files: {
-          'public/lib/react-router/reactRouter.js': 'https://npmcdn.com/react-router@2.3.0/umd/ReactRouter.js'
-          // 'public/lib/history/history.js': 'https://npmcdn.com/history@2.1.1/umd/history.js'
-        }
-    }
-};
