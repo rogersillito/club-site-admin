acc7d801624f026193d78408a3a1f2e404e526e5
rogersillito <rogersillito@gmail.com>
2016-12-22 11:17:26 +0000

    added updates for MenuLinks to existing system-managed pages.  Added note on relative url usage for MenuLink.  Changed blog url to posts.

--- routes/index.js
+++ routes/index.js
@@ -54,8 +54,8 @@ exports = module.exports = function(app) {
   
   // Views
   app.get('/', routes.views.index);
-  app.get('/blog/:category?', routes.views.blog);
-  app.get('/blog/post/:post', routes.views.post);
+  app.get('/posts/:category?', routes.views.blog);
+  app.get('/post/post/:post', routes.views.post);
   app.get('/gallery', routes.views.gallery);
   app.get('/results', routes.views.results);
   app.get('/results/*', routes.views.results); // react-router handles these
---
+++ updates/0.0.3-add-menu-links.js
@@ -0,0 +1,12 @@
+/**
+ * create homepage and settings instances
+ */
+
+exports.create = {
+	MenuLink: [
+		{ 'title': 'News', 'relativeUrl': '/posts', 'navOrder': 1 },
+		{ 'title': 'Results', 'relativeUrl': '/results', 'navOrder': 2 },
+		{ 'title': 'Gallery', 'relativeUrl': '/gallery', 'navOrder': 3 },
+		{ 'title': 'Contact Us', 'relativeUrl': '/contact', 'navOrder': 4 },
+	]
+};
