06cf68c1caaa7713f9a77e5e3c20ede1c45ffb3a
rogersillito <rogersillito@gmail.com>
2017-04-12 22:15:09 +0100

    posts now show a limited summary in the posts lists, either the content brief or a truncated content extended.  If brief is too long it fails to save

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,22 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
+var striptags = require('striptags');
+var _ = require('underscore');
+
+function wordLimit(text, limit) {
+  var words =  text.replace(/\s+/g,' ').trim().split(' ');
+  var limited = false;
+  return _.reduce(words, function(m,word,i) {
+    var isLast = i === words.length-1;
+    if (i >= limit) {
+      return (isLast && limited) ? m + '...' : m;
+    }
+    if (i === limit-1)  {
+      limited = true;
+    }
+    return m + ' ' + word;
+  },'').substr(1);
+}
 
 /**
  * Post Model
@@ -11,6 +28,7 @@ var Post = new keystone.List('Post', {
 	autokey: { path: 'slug', from: 'title', unique: true }
 });
 
+var summaryLimit = 60;
 Post.add({
 	title: { type: String, required: true },
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true, required: true, initial: true },
@@ -19,15 +37,35 @@ Post.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage, autoCleanup: true, folder: 'posts'  },
 	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-    //TODO: make this a watch field that word restricts, respecting html
-		// brief: { type: Types.Html, watch: true, height: 150 },
+		brief: { type: Types.Html, wysiwyg: true, height: 150, note: 'If completed, this will display in the list of posts beneath the title - otherwise Content Extended will be limited to ' + summaryLimit + ' words and displayed in its place.' },
+		summary: { type: String, watch: true, hidden: true, value: function() {
+      if (this.content.brief.trim()) {
+        return this.content.brief;
+      }
+      var extendedText = striptags(this.content.extended);
+      return wordLimit(extendedText, summaryLimit);
+    } },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 }
 	}
 });
 
 Post.schema.virtual('content.full').get(function() {
 	return this.content.extended || this.content.brief;
+});
+
+Post.schema.pre('save', function(next) {
+  var contentBrief = this.content.brief.trim();
+  if (contentBrief) {
+    var stripped = striptags(contentBrief);
+    var limited = wordLimit(stripped, summaryLimit);
+    var unlimited = wordLimit(stripped, summaryLimit + 1);
+    console.log("unlimited = ", unlimited);
+    console.log("limited = ", limited);
+    if (limited !== unlimited) {
+      next(new Error('Content Brief should not be longer than ' + summaryLimit + ' words.'));
+    }
+  }
+  next();
 });
 
 Post.defaultColumns = 'title|30%, categories|20%, state, author, publishedDate';
