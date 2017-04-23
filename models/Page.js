8dc7e801b98af1caa191f5d95b6c9bdab1a7de43
rogersillito <rogersillito@gmail.com>
2017-04-23 21:01:41 +0100

    added 3 image fields to Page model, with cloudinary cleanup

--- models/Page.js
+++ models/Page.js
@@ -2,18 +2,24 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;  
 var NavNode = require('./NavNode');
 var addNavNodeChildBehaviours = require('../lib/navNodeChildMixin.js');
+var addCloudinaryCleanupBehaviours = require('../lib/addCloudinaryCleanupBehaviours.js');
 
 var Page = new keystone.List('Page', {
   inherits: NavNode,
 
   hidden: false
 });
+var folder = 'pages';
 Page.add({  
   isPublished: { type: Boolean, label: 'Is Published?', index: true, note: 'If this page has any child pages, un-publishing it will cause any child pages to be unpublished too!' },
   publishedDate: { type: Date, default: Date.now },
   metaDescription: { type: String },
-  content: { type: Types.Html, wysiwyg: true }
+  content: { type: Types.Html, wysiwyg: true },
+  image1: { type: Types.CloudinaryImage, autoCleanup: true, folder: folder  },
+  image2: { type: Types.CloudinaryImage, autoCleanup: true, folder: folder  },
+  image3: { type: Types.CloudinaryImage, autoCleanup: true, folder: folder  }
 });
+addCloudinaryCleanupBehaviours(Page);
 Page.schema.statics.view_name = 'page';  
 
 addNavNodeChildBehaviours(Page);
