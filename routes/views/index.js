677f478a39b1ad93e439e4433b777f29b372825a
rogersillito <rogersillito@gmail.com>
2017-05-29 19:09:09 +0100

    added homepage banner logo/html

--- routes/views/index.js
+++ routes/views/index.js
@@ -7,6 +7,8 @@ exports = module.exports = function(req, res) {
 	view.on('init', function(next) {
     var view = new keystone.View(req, res);
     var locals = res.locals;
+	  locals.bannerTitle = `<span class="home-banner">Welcome to<br /> <span><img src="/images/logo-home-banner.png" alt="${locals.siteName}"></span> <small class="tagline">Founded 1985</small></span>`;
+    locals.pageTitle = 'Home';
     next();
   });
 
