f0f517ab7fb30ba274fe7aaba071746f2809fd84
rogersillito <rogersillito@gmail.com>
2017-07-19 21:25:05 +0100

    script for recreating openshift app

---
+++ tools/openshift2-create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+app=site
+nodecart=nodejs-0.10
+
+# delete existing
+rhc app delete $app --confirm
+
+# create app with mongodb and scaleable node cartridge
+rhc app-create $app $nodecart -s
+rhc cartridge-add mongodb-2.4 -a $app -g small
+
+# add storage to node cart
+rhc cartridge-storage $nodecart -a $app --add 3GB
+
+#TODO: remove default repo contents: cd $app, rm -rf etc..
