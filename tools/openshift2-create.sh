57d17b86732975afaa04c357932ce63ef9b17b14
rogersillito <rogersillito@gmail.com>
2017-07-22 18:43:54 +0100

    openshift create script now removes default initial repo contents

--- tools/openshift2-create.sh
+++ tools/openshift2-create.sh
@@ -13,5 +13,13 @@ rhc cartridge-add mongodb-2.4 -a $app -g small
 # add storage to node cart
 rhc cartridge-storage $nodecart -a $app --add 3GB
 
-#TODO: remove default repo contents: cd $app, rm -rf etc..
-#      then remove $app dir the above commands creates!
+# remove default repo contents and push
+cd ./$app
+git rm -r '*'
+git commit -m 'remove default openshift repo contents'
+git push -u origin master
+
+# remove repo created by rhc
+cd ..
+rm -rf ./$app
+
