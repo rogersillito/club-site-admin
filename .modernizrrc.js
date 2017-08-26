c95a36a6af9ebc60afb7885b5b824e6708e4ccab
rogersillito <rogersillito@gmail.com>
2017-08-26 12:51:16 +0100

    forgot to add modernizr settings file

---
+++ .modernizrrc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  options: [
+		// Full list of supported options can be found in:
+		// https://github.com/Modernizr/Modernizr/blob/master/lib/config-all.json
+    "setClasses"
+  ],
+  "feature-detects": [
+    "test/css/rgba"
+  ]
+};
