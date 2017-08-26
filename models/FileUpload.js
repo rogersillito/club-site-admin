f71ce172942eb5c685bc85ac77d0bc1515846b18
rogersillito <rogersillito@gmail.com>
2017-08-26 19:16:11 +0100

    S3 cleanup behaviour now working on upload of new file version.  Add version and improved upload date format fields

--- models/FileUpload.js
+++ models/FileUpload.js
@@ -3,22 +3,42 @@ var Types = keystone.Field.Types;
 var _ = require('underscore');
 var path = require('path');
 var AWS = require('aws-sdk');
+var moment = require('moment');
+var dateHelpers = require('../lib/dateHelpers');
 
 var FileUpload = new keystone.List('FileUpload', {
 	autokey: { from: 'name', path: 'fileId', unique: true }
 });
 
+const timestampFormat = dateHelpers.formatStrings.fileTimestamp;
 FileUpload.add({
 	name: {
 		type: String,
+		// noedit: true,  // allows filename/doc name to get out of sync - but probably useful to be able to edit public name
 		required: true
 	},
 	uploadDate: {
-		type: Types.Datetime,
-		default: Date.now,
+		type: String,
+		default: '',
 		noedit: true
 	},
-	publishedDate: { type: Types.Datetime, hidden: true, index: true },
+	originalName: {
+		label: 'Uploaded Filename',
+		type: String,
+		noedit: true,
+		watch: true,
+		value: function() { return this.file && this.file.originalname; }
+	},
+	version: {
+		type: Types.Number,
+		noedit: true,
+		default: 0
+	},
+	requireFile: {
+		type: Types.Boolean,
+		hidden: true,
+		default: false
+	},
 	file: {
 		type: Types.S3File,
 		label: 'File',
@@ -45,20 +65,15 @@ FileUpload.add({
 		],
 		filename: (doc, filename) => {
 			var ext = path.extname(filename);
-			var ts = doc._.uploadDate.format('YYYY-MM-DD_hh-mm-ss_');
+			// var ts = doc._.uploadDate.format('YYYY-MM-DD_hh-mm-ss_');
+			var ts = moment().format(timestampFormat + '_');
 			var name = ts + doc.fileId + ext;
 			return name;
 		}
-	},
-	originalName: {
-		label: 'Uploaded Filename',
-		type: String,
-		noedit: true,
-		watch: true,
-		value: function() { return this.file && this.file.originalname; }
 	}
 });
 
+// helper methods for interacting directly with S3:
 const s3Options = {
 	accessKeyId: process.env.S3_KEY,
 	secretAccessKey: process.env.S3_SECRET,
@@ -67,42 +82,75 @@ const s3Options = {
 AWS.config.update(s3Options);
 var s3Client = new AWS.S3();
 
-FileUpload.schema.pre('save', function(next) {
-  this.uploadDate = Date.now();
-	if (this.file && this.file.url === '') {
-		// prevent removal of contained S3 file
-    next(new Error('If you wish to remove this file, please remove the FileUpload itself (click "delete file upload") rather than the file you have attached to it.'));
+const deleteS3File = filename => {
+	const deleteParams = {
+		Bucket: process.env.S3_BUCKET,
+		Delete: {
+			Objects: [{ Key: filename }]
+		}
+	};
+	s3Client.deleteObjects(deleteParams, (err, data) => {
+		if (err) {
+			console.error(err, err.stack);
+			return;
+		}
+		const msg = _.find(data.Deleted, obj => obj.Key === filename) ? "Cleaned up" : "Failed to delete";
+		console.log(msg + ' S3 file: ' + filename);
+	});
+};
+
+
+
+// MIDDLEWARE
+var initialS3Filename = undefined;
+FileUpload.schema.post('init', function(doc) {
+	if (doc.file) {
+		initialS3Filename = doc.file.filename;
 	}
+});
+
+FileUpload.schema.pre('save', function(next) {
+	if (this.requireFile) {
+		if (this.file && (typeof(this.file.url) === 'undefined' || this.file.url === '')) {
+			// prevent removal of contained S3 file, or save without file (except for initial creation of doc)
+			next(new Error('FILE IS REQUIRED: If you wish to remove this uplaod, please remove the File Upload entry itself (click "delete file upload") rather than the file you have attached to it.'));
+		}
+	}	else {
+		this.requireFile = true; // next save requires a file!
+	}
+
+  this.uploadDate = '';
+	if (this.file) {
+		if (this.file.filename) {
+			// set uploadDate based on filename timestamp
+			const filnameTs = this.file.filename.substr(0,timestampFormat.length);
+			const filetime = moment(filnameTs, timestampFormat);
+			const formatted = filetime.format(dateHelpers.formatStrings.dayDateTimeString);
+			this.uploadDate = formatted;
+
+			if (initialS3Filename !== this.file.filename) {
+				// file changed - update version
+				this.version = this.version + 1;
+			}
+		}
+	}
+
 	return next();
+});
+
+FileUpload.schema.post('save', function(doc) {
+	if (initialS3Filename && doc.file && initialS3Filename !== doc.file.filename) {
+		// new file uploaded: cleanup previously uploaded version in S3
+		deleteS3File(initialS3Filename);
+	}
 });
 
 FileUpload.schema.post('remove', function(doc) {
 	if (doc.file && doc.file.filename) {
-		const filename = doc.file.filename;
-		// cleanup file in S3
-		const deleteParams = {
-			Bucket: process.env.S3_BUCKET,
-			Delete: {
-				Objects: [{ Key: filename }]
-			}
-		};
-		s3Client.deleteObjects(deleteParams, (err, data) => {
-			if (err) {
-				console.error(err, err.stack);
-				return;
-			}
-			const msg = _.find(data.Deleted, obj => obj.Key === filename) ? "Cleaned up" : "Failed to delete";
-			console.log(msg + ' S3 file: ' + filename);
-		});
+		// file deleted: cleanup file in S3
+		deleteS3File(doc.file.filename);
 	}
 });
 
-FileUpload.schema.post('save', function(doc) {
-	//TODO: TEST IF... need to do any cleanup if file changes in some way?
-	if (doc.file) {
-		// console.log("doc.file = ", this.file);
-	}
-});
-
-FileUpload.defaultColumns = 'name, publishedState, publishedDate';
+FileUpload.defaultColumns = 'name';
 FileUpload.register();
